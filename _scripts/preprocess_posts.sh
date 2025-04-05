398ed6ad00cd9f7d81a45428d45b66d71413172c
Tim Brookes <Timothy.Brookes@gmail.com>
2025-04-05 10:29:58 +0000

    feat: added mermaid support

--- _scripts/preprocess_posts.sh
+++ _scripts/preprocess_posts.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 set -e
 
-# Set the baseurl
-BASEURL="{{ site.baseurl }}"
-
 # Create _posts/ if it doesn't exist
 mkdir -p _posts
 
-# Process each Markdown file
+# Inject baseurl into all assets
+BASEURL="{{ site.baseurl }}"
 for filepath in _posts_md/*.md; do
   filename=$(basename "${filepath}")
-  sed -E "s#(!\[.*\]\()\/assets/#\1${BASEURL}/assets/#g" "${filepath}" > "_posts/${filename}"
+  sed -E "s#(!\[.*\]\()/assets/#\1${BASEURL}/assets/#g" "${filepath}" > "_posts/${filename}"
 done
 
-echo "Preprocessed markdown with baseurl to _posts/"
+# Replace mermaid code blocks with HTML
+for filepath in _posts/*.md; do
+  sed -i -E '/```mermaid/,/```/{
+    s/```mermaid/<div class="mermaid">/
+    s/```/<\/div>/
+  }' "${filepath}"
+done
+
+echo "Preprocessed markdown files"
